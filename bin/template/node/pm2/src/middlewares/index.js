c996fe09f50423b4c5ddd5959fb5ee92c11c5f80
AlpoxDev <alpoxdev@gmail.com>
2022-07-12 03:44:32 +0900

    init

--- bin/template/node/pm2/src/middlewares/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authorization = exports.UserType = exports.jwtUserCallback = void 0;
-var authorization_1 = require("./authorization");
-exports.authorization = authorization_1.default;
-var jwtUserCallback_1 = require("./jwtUserCallback");
-Object.defineProperty(exports, "jwtUserCallback", { enumerable: true, get: function () { return jwtUserCallback_1.default; } });
-Object.defineProperty(exports, "UserType", { enumerable: true, get: function () { return jwtUserCallback_1.UserType; } });
